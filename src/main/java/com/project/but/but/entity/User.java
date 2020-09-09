3781d1103435001a902d50558b5ece7890d335a7
moon-1 <mhwgodnjs@naver.com>
2020-09-09 21:21:24 +0900

    Update user database api

--- src/main/java/com/project/but/but/entity/User.java
+++ src/main/java/com/project/but/but/entity/User.java
@@ -2,15 +2,19 @@ package com.project.but.but.entity;
 
 import lombok.Data;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Data
 @Entity
 public class User {
     @Id
-    private int userNum;
-    private String userId;
+    @GeneratedValue
+    @Column(nullable = false)
+    private int user_num;
+    private String user_id;
     private String password;
     private String email;
     private String phone;
